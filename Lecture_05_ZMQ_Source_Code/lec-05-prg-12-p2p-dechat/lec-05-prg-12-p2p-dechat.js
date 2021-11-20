9570ab4bf8443cf94e2d8d17bc78e19e07bd42da
KimDaeSeon <kimdaeseon@khu.ac.kr>
2021-11-21 02:54:39 +0900

    solve sample 12 error

--- Lecture_05_ZMQ_Source_Code/lec-05-prg-12-p2p-dechat/lec-05-prg-12-p2p-dechat.js
+++ Lecture_05_ZMQ_Source_Code/lec-05-prg-12-p2p-dechat/lec-05-prg-12-p2p-dechat.js
@@ -5,61 +5,21 @@ const ip = require('ip')
 const search_nameserver = async (ip_mask, local_ip_addr, port_nameserver) =>{
     const subscribeSocket = new zmq.Subscriber
     result = ""
-    for (let i = 0 ; i < 4; i++){
+    for (let i = 0 ; i < 10; i++){
         let target_ip_addr = "tcp://"+String(ip_mask)+"."+String(i)+":"+port_nameserver
-        if(target_ip_addr != local_ip_addr){
+        if(target_ip_addr != local_ip_addr || target_ip_addr == local_ip_addr){
             subscribeSocket.connect(target_ip_addr)
         }
         subscribeSocket.receiveTimeout = 200
         subscribeSocket.subscribe("NAMESERVER")
         await subscribeSocket.receive().then((msg)=>{
-            result = msg.toString()
+            result = msg.toString().split(":")[1]
         }, (error) => {
             result = ""
         })
         if (result != ""){
             return result
         }
-    }
-}
-
-const beacon_nameserver = async (local_ip_addr, port_nameserver)=>{
-    const publishSocket = new zmq.Publisher
-    await publishSocket.bind("tcp://"+local_ip_addr+":"+port_nameserver)
-    console.log("loacl p2p name server bind to tcp://"+local_ip_addr+":"+port_nameserver+".")
-    while(true){
-        await new Promise(resolve=>setTimeout(resolve, 1000))
-        const msg = "NAMESERVER:"+local_ip_addr
-        publishSocket.send(msg)
-    }
-}
-
-const user_manager_nameserver = async (local_ip_addr, port_subscribe)=>{
-    const user_db = []
-    const replySocket = new zmq.Reply
-    await replySocket.bind("tcp://"+local_ip_addr+":"+port_subscribe)
-    console.log("local p2p db server activated at" + "tcp://"+local_ip_addr+":"+port_subscribe)
-    while(true){
-        const raw_user_request = await replySocket.receive()
-        const user_request = raw_user_request.toString().split(":")
-        user_db.push(user_request)
-        console.log("user registration '" + user_request[1] + "' from '" + user_request[0] +"'.")
-        replySocket.send("ok")
-    }
-}
-
-const relay_server_nameserver = async (local_ip_addr, port_chat_publisher, port_chat_collector)=>{
-    const publishSocket = new zmq.Publisher
-    await publishSocket.bind("tcp://"+local_ip_addr+":"+port_chat_publisher)
-    const pullSocket = new zmq.Puller
-    await pullSocket.bind("tcp://"+local_ip_addr+":"+port_chat_collector)
-    console.log("local p2p relay server activated at" + "tcp://"+local_ip_addr+":"+port_chat_publisher +"&" + port_chat_collector)
-
-    while(true){
-        const raw_message = await pullSocket.receive()
-        const message = raw_message.toString()
-        console.log("p2p-relay:<==>", message)
-        publishSocket.send("RELAY:" + message)
     }
 }
 
@@ -103,9 +63,9 @@ const app = async (argv)=>{
     const p2p_rx = new zmq.Subscriber
     p2p_rx.subscribe("RELAY")
     p2p_rx.receiveTimeout = 100
-    p2p_rx.connect("tcp://"+ip_addr_p2p_server+":"+port_chat_publisher)
+    await p2p_rx.connect("tcp://"+ip_addr_p2p_server+":"+port_chat_publisher)
     const p2p_tx = new zmq.Push
-    p2p_tx.connect("tcp://"+ip_addr_p2p_server+":"+port_chat_collector)
+    await p2p_tx.connect("tcp://"+ip_addr_p2p_server+":"+port_chat_collector)
     
     console.log("starting autonomous message transmit and receive scenario. ")
 
